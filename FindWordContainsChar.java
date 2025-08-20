508e0d163a2726f32f08cddb4b41068668d5209c
pratik9905 <pratikkumar734@gmail.com>
2025-08-20 22:38:52 +0530

    find-words-containing-character

---
+++ FindWordContainsChar.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindWordContainsChar {
+    public static void main(String[] args) {
+        String[] str = {"abc","bcd","aaaa","cbc"};
+        char x= 'a';
+        System.out.println(findWordsContaining(str,x));
+    }
+
+    public static List<Integer> findWordsContaining(String[] str, char x){
+        int n=str.length;
+        List<Integer> ls = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(str[i].contains(String.valueOf(x)))
+             ls.add(i);
+        }
+        return ls;
+    }
+    
+}
